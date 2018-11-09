8e4a0b9146cc8e5ddf2af8ce2420c9117c28f1ac
Rohan <rrnarayan1@gmail.com>
2018-11-09 11:29:57 -0800

    fixed static pages, about us and contact us page built

--- shared/nav.js
+++ shared/nav.js
@@ -14,10 +14,11 @@ var dj_file = folder == "up" ? "../data-journalism.html" : "data-journalism.html
 var projects_file = folder == "up" ? "../projects.html" : "projects.html"
 var blog_file = folder == "up" ? "../blog.html" : "blog.html"
 var about_file = folder == "up" ? "../about-us.html" : "about-us.html"
+var contact_file = folder == "up" ? "../contact-us.html" : "contact-us.html"
 
 var string =
 "  <ul>"+
-"    <li "+home+"><a href="+index_file+">Home</a></li>"+
+//"    <li "+home+"><a href="+index_file+">Home</a></li>"+
 "    <li "+dj+"><a href="+dj_file+">Data Journalism</a></li>"+
 "    <li "+projects+"><a href="+projects_file+">Projects</a></li>"+
 "    <li "+blog+"><a href="+blog_file+">Blog</a></li>"+
@@ -25,8 +26,8 @@ var string =
 "      <a href='#'>About</a>"+
 "      <ul>"+
 "        <li><a href="+about_file+">About Us</a></li>"+
-"        <li><a href='#'>Contact Us</a></li>"+
-"        <li><a href='#'>Leadership</a></li>"+
+"        <li><a href="+contact_file+">Contact Us</a></li>"+
+//"        <li><a href='#'>Leadership</a></li>"+
 "      </ul>"+
 "    </li>"+
 "  </ul>"
