031b1dc11846134885a88f6529c786e38cbb62bb
Chloe Lo <chloelo08@yahoo.com>
2019-04-22 22:16:51 -0700

    made some changes to index page, created page for SAGB academic prize

--- shared/footer.js
+++ shared/footer.js
@@ -5,36 +5,31 @@ var folder = el.getAttribute("folder");
 var index_file = folder == "up" ? "../index.html" : "index.html"
 var image_file = folder == "up" ? "../images/logo1.png" : "images/logo1.png"
 
-var string = `          <footer class="container" id="footer">
+var string = `    
+          <footer class="container" id="footer">
             <div class="row">
               <div class="col-6 col-12-medium imp-medium">
-
-                <!-- Contact -->
-                  <section>
-                    <h2>Get in touch</h2>
-                    <div>
-                      <div class="row">
-                        <div class="col-6 col-12-small">
-                          <dl class="contact">
-                            <dt>Twitter</dt>
-                            <dd><a href="https://twitter.com/sagberkeley">@sagberkeley</a></dd>
-                            <dt>Facebook</dt>
-                            <dd><a href="https://facebook.com/SportsAnalyticsBerkeley">facebook.com/SportsAnalyticsBerkeley</a></dd>
-                            <dt>WWW</dt>
-                            <dd><a href=`+index_file+`>berkeleysportsanalytics.org</a></dd>
-                            <dt>Email</dt>
-                            <dd><a>sagberkeley@gmail.com</a></dd>
-                          </dl>
-                        </div>
-                      </div>
+              <!-- Contact -->
+              <section>
+                <h2>Get in touch</h2>
+                <div>
+                  <div class="row">
+                    <div class="col-8 col-12-medium">
+                      <dl class="contact tab">
+                        <dt><a href="https://twitter.com/sagberkeley" class="fa fa-twitter"></a></dt>
+                        <dt><a href="https://facebook.com/SportsAnalyticsBerkeley" class="fa fa-facebook"></a></dt>
+                        <dt><a href="mailto:sagberkeley@gmail.com" class="fa fa-envelope"></a></dt>
+                      </dl>
                     </div>
-                  </section>
+                  </div>
+                </div>
+              </section>
 
               </div>
               <div class="col-1 col-12-small hide-mobile">
                 <span> </span>
               </div>
-              <div class="col-4 col-12-small hide-mobile">
+              <div class="col-3 col-12-small hide-mobile">
                 <img class="footer-image" src=`+image_file+` />
               </div>
               <div class="col-12">
