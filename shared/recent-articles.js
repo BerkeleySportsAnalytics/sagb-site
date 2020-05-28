86bf28ccbebb2e94817c8d5c49cc7b74036a1cb9
Chloe Lo <chloelo08@yahoo.com>
2020-05-28 12:20:41 -0700

    check for issues with article list

--- shared/recent-articles.js
+++ shared/recent-articles.js
@@ -1,25 +1,25 @@
 var el = document.getElementById("recent-articles-src");
 var folder = el.getAttribute("folder");
 
-article1 = "is-injury-prediction-the-next-moneyball.html";
-article2 = "clayton-kershaw-playoff-choker.html";
-article3 = "jeopardy-to-vegas.html";
-article4 = "speed-kills.html";
-article5 = "weight-theres-more.html";
-article6 = "new-age-moneyball.html";
-article7 = "point-centers.html";
-article8 = "patriots-historical-domination.html";
-article9 = "nba-free-agency-2016.html";
+article1 = "cant-stand-the-heat.html";
+article2 = "is-injury-prediction-the-next-moneyball.html";
+article3 = "clayton-kershaw-playoff-choker.html";
+article4 = "jeopardy-to-vegas.html";
+article5 = "speed-kills.html";
+article6 = "weight-theres-more.html";
+article7 = "new-age-moneyball.html";
+article8 = "point-centers.html";
+article9 = "patriots-historical-domination.html";
 
-title1 = "Is Injury Prediction the Next Moneyball?";
-title2 = "Clayton Kershaw: Playoff Choker?";
-title3 = "America's Game Goes to Vegas: How to bet on the Jeopardy! GOAT";
-title4 = "Speed Kills?";
-title5 = "But Weight, There’s More";
-title6 = "New-Age Moneyball: Breaking Down How Two Low-Budget Teams Made the American League Wild Card Game";
-title7 = "Point Centers, a New Breed in the NBA";
-title8 = "The New England Patriots: Historical Domination";
-title9 = "Blew it All on Luol? Turned off by Mozgov?: Statistical Analysis of 2016 NBA Free Agency";
+title1 = "Can't Stand the Heat?";
+title2 = "Is Injury Prediction the Next Moneyball?";
+title3 = "Clayton Kershaw: Playoff Choker?";
+title4 = "America's Game Goes to Vegas: How to bet on the Jeopardy! GOAT";
+title5 = "Speed Kills?";
+title6 = "But Weight, There’s More";
+title7 = "New-Age Moneyball: Breaking Down How Two Low-Budget Teams Made the American League Wild Card Game";
+title8 = "Point Centers, a New Breed in the NBA";
+title9 = "The New England Patriots: Historical Domination";
 
 article1 = folder == "down" ? "articles/"+article1 : article1;
 article2 = folder == "down" ? "articles/"+article2 : article2;
