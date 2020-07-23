46347a07b6ba6aaf942f4ad17dcb908b2a9382b5
Kunal Dutta <kdutta@berkeley.edu>
2020-07-23 16:20:33 -0700

    added player image to 2nd column

--- dfs/main.js
+++ dfs/main.js
@@ -23,7 +23,7 @@ function teamSearch() {
 	table = document.getElementById("tbl");
 	tr = table.getElementsByTagName("tr");
 	for (i = 0; i < tr.length; i++) {
-	  td = tr[i].getElementsByTagName("td")[3];
+	  td = tr[i].getElementsByTagName("td")[4];
 	  if (td) {
 		txtValue = td.textContent || td.innerText;
 		if (txtValue.toUpperCase().indexOf(filter) > -1) {
@@ -41,7 +41,7 @@ function posSearch() {
 	table = document.getElementById("tbl");
 	tr = table.getElementsByTagName("tr");
 	for (i = 0; i < tr.length; i++) {
-	  td = tr[i].getElementsByTagName("td")[1];
+	  td = tr[i].getElementsByTagName("td")[2];
 	  if (td) {
 		txtValue = td.textContent || td.innerText;
 		if (txtValue.toUpperCase().indexOf(filter) > -1) {
