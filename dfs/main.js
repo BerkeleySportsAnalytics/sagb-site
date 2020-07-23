40623dda8b0f7ed1fc326dd6513abcef8f35733d
Kunal Dutta <kdutta@berkeley.edu>
2020-07-23 15:50:11 -0700

    added position filter

--- dfs/main.js
+++ dfs/main.js
@@ -34,3 +34,21 @@ function teamSearch() {
 		}
 	}
 }
+function posSearch() {
+	var input, filter, table, tr, td, i, txtValue;
+	input = document.getElementById("position");
+	filter = input.value.toUpperCase();
+	table = document.getElementById("tbl");
+	tr = table.getElementsByTagName("tr");
+	for (i = 0; i < tr.length; i++) {
+	  td = tr[i].getElementsByTagName("td")[1];
+	  if (td) {
+		txtValue = td.textContent || td.innerText;
+		if (txtValue.toUpperCase().indexOf(filter) > -1) {
+	   tr[i].style.display = "";
+	   } else {
+	   tr[i].style.display = "none";
+			}
+		}
+	}
+}
