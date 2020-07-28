b996fbfa99144b86d58ddd860cad201ffab8ae84
Kunal Dutta <kdutta@berkeley.edu>
2020-07-27 18:35:28 -0700

    added positional dropdown filter

--- dfs/main.js
+++ dfs/main.js
@@ -35,20 +35,19 @@ function teamSearch() {
 	}
 }
 function posSearch() {
-	var input, filter, table, tr, td, i, txtValue;
-	input = document.getElementById("position");
-	filter = input.value.toUpperCase();
-	table = document.getElementById("tbl");
-	tr = table.getElementsByTagName("tr");
-	for (i = 0; i < tr.length; i++) {
-	  td = tr[i].getElementsByTagName("td")[2];
-	  if (td) {
-		txtValue = td.textContent || td.innerText;
-		if (txtValue.toUpperCase().indexOf(filter) > -1) {
-	   tr[i].style.display = "";
-	   } else {
-	   tr[i].style.display = "none";
-			}
-		}
-	}
+  var input, filter, table, tr, td, i;
+  input = document.getElementById("pos");
+  filter = input.value.toUpperCase();
+  table = document.getElementById("tbl");
+  tr = table.getElementsByTagName("tr");
+  for (i = 0; i < tr.length; i++) {
+    td = tr[i].getElementsByTagName("td")[2];
+    if (td) {
+      if (td.innerHTML.toUpperCase().indexOf(filter) > -1) {
+        tr[i].style.display = "";
+      } else {
+        tr[i].style.display = "none";
+      }
+    }       
+  }
 }
