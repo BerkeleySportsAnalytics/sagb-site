fe7416d96446f6abcadfeefc73f76a3a07e6bcac
Henry Jia <hjia3@berkeley.edu>
2023-01-15 19:51:49 -0800

    initial updates + new articles

--- shared/featured-articles.js
+++ shared/featured-articles.js
@@ -1,24 +1,24 @@
 var el = document.getElementById("featured-article-src");
 var folder = el.getAttribute("folder");
 
-article1 = "hands_up";
-article2 = "break-points";
-article3 = "mlb-day-night";
+article1 = "2021-qb-class";
+article2 = "japan-to-mlb";
+article3 = "aaron-judge-article";
 article4 = "march-madness"
 
-title1 = "Hands Up";
-title2 = "BREAK POINTS: WHAT’S THE KEY?";
-title3 = "HYPOTHESIS TESTING MLB HITTERS IN DAY AND NIGHT GAMES";
+title1 = "Analyzing the 2021 QB class so far: Which QB has looked the best this year and overall?";
+title2 = "From Japan to MLB: The Players Before and After Signing";
+title3 = "All Rise: Judge set to Break Precedent this Offseason";
 title4 = "A SANE RETROSPECTIVE OF THE MADNESS OF MARCH"
 
-date1 = "June 6, 2021";
-date2 = "March 30, 2021";
-date3 = "May 20, 2021";
-date4 = "May 16, 2021"
+date1 = "December 7, 2022";
+date2 = "November 29, 2022";
+date3 = "December 28, 2022";
+date4 = "November 11, 2022"
 
-excerpt1 = "A couple of years ago, Bob Myers, the general manager of the Golden State Warriors, described that his “ideal player would be 6 foot 8 with a 7 foot 6 wingspan”...";
-excerpt2 = "One of the most nerve wracking situations as a tennis player is being faced with a breakpoint. Your opponent seemingly has all the momentum and they know it."
-excerpt3 = "Prior to 1935, all Major League Baseball games were played during the afternoon. When the night game was introduced, it added a new aspect to the sport...";
+excerpt1 = "The 2021 Quarterback class was one of the most hyped up Quarterback draft classes in recent memory. The buzz leading up to draft night last year was incredible as there were...";
+excerpt2 = "Shohei Ohtani became the “Face of Baseball” in his 2021 MVP season, where he posted elite numbers on the mound and at the plate. However, Ohtani’s success drew attention to his former league, Nippon Professional Baseball..."
+excerpt3 = "Aaron Judge put up a historic rookie season in 2017 and - after a narrow race with Jose Altuve - almost won the two most prestigious individual awards for a player, MVP and Rookie of the Year, in his first full year in the league...";
 excerpt4 = "The NCAA tournament this year brought many surprises. From number 11 seed UCLA making it to the final four and losing on a fluke half courter by Gonzaga guard Jalen Suggs to the magic of Max Abmas..."
 
 file1 = folder == "down" ? "articles/"+article1+".html" : article1+".html";
@@ -32,9 +32,9 @@ im_file1 = folder == "down" ? `images/dj-pics/`+article1+`/Picture1.png` : `../i
 im_file2 = folder == "down" ? `images/dj-pics/`+article2+`/Picture1.png` : `../images/dj-pics/`+article2+`/Image1.PNG`;
 im_file3 = folder == "down" ? `images/dj-pics/`+article3+`/Picture1.png` : `../images/dj-pics/`+article3+`/Image1.PNG`;
 */
-im_file1 = `../images/dj-pics/`+article1+`/Image1.PNG`;
-im_file2 = `../images/dj-pics/`+article2+`/Image1.PNG`;
-im_file3 = `../images/dj-pics/`+article3+`/Image1.PNG`;
+im_file1 = `../images/dj-pics/`+article1+`/main.jpeg`;
+im_file2 = `../images/dj-pics/`+article2+`/main.jpg`;
+im_file3 = `../images/dj-pics/`+article3+`/main.jpg`;
 im_file4 = `../images/dj-pics/`+article4+`/Image1.PNG`;
 
 
