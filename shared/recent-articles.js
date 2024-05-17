133b7bf0f61a924fdd2855c6f25a9c3e1e645e0c
aschlaton <84212237+aschlaton@users.noreply.github.com>
2024-05-17 00:31:39 -0400

    added articles

--- shared/recent-articles.js
+++ shared/recent-articles.js
@@ -1,25 +1,25 @@
 var el = document.getElementById("recent-articles-src");
 var folder = el.getAttribute("folder");
 
-article1 = "bowl_or_not.html";
-article2 = "skating-dead-legs.html";
-article3 = "ncaa-wrapped.html";
-article4 = "TotalEclipse.html";
-article5 = "todd-boehly.html";
-article6 = "basketball-genetics.html";
-article7 = "fantasy-predictions.html";
-article8 = "nfl-combine.html";
-article9 = "uswnt-struggles.html";
+article1 = "defense-strikes-harder.html";
+article2 = "transfer-values.html";
+article3 = "bowl_or_not.html";
+article4 = "skating-dead-legs.html";
+article5 = "ncaa-wrapped.html";
+article6 = "TotalEclipse.html";
+article7 = "todd-boehly.html";
+article8 = "basketball-genetics.html";
+article9 = "fantasy-predictions.html";
 
-title1 = "To Bowl or not to Bowl";
-title2 = "Skating on Dead Legs: Designing a Search that Prioritizes Player Health and Efficient Travel";
-title3 = "NCAA 2013 – 2023 Wrapped: The Best and Worst Games of the Past Decade";
-title4 = "The Total Eclipse has Arrived";
-title5 = "Todd Boehly's New Chelsea";
-title6 = "The Gender of Genetics in Basketball";
-title7 = "Fantasy Football: Predicting Wide Receiver Perfomances with Statistics";
-title8 = "How Important are NFL Combine Performances?";
-title9 = "Struggles of the USWNT: A Deeper Look into the 2023 Women's Wos World Cup";
+title1 = "When the Defense Strikes Harder";
+title2 = "Decoding TransfrMarket: Analyzing Player Values Versus Player Performance";
+title3 = "To Bowl or not to Bowl";
+title4 = "Skating on Dead Legs: Designing a Search that Prioritizes Player Health and Efficient Travel";
+title5 = "NCAA 2013 – 2023 Wrapped: The Best and Worst Games of the Past Decade";
+title6 = "The Total Eclipse has Arrived";
+title7 = "Todd Boehly's New Chelsea";
+title8 = "The Gender of Genetics in Basketball";
+title9 = "Fantasy Football: Predicting Wide Receiver Perfomances with Statistics";
 
 article1 = folder == "down" ? "articles/"+article1 : article1;
 article2 = folder == "down" ? "articles/"+article2 : article2;
