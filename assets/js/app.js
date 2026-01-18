45e952d5948946818d4c527ae674c78684f496c7
DesertGaming <ojpereira4@gmail.com>
2026-01-18 00:26:20 -0800

    continued testing and recruitment updates

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,4 +1,4 @@
-const observer = new IntersectionObserver((entries) => {
+/*const observer = new IntersectionObserver((entries) => {
     entries.forEach((entry) => {
         console.log(entry)
         if (entry.isIntersecting) {
@@ -12,4 +12,22 @@ const observer = new IntersectionObserver((entries) => {
 
 
 const hiddenElements = document.querySelectorAll('.hidden');
-hiddenElements.forEach((el) => observer.observe(el));
+hiddenElements.forEach((el) => observer.observe(el));*/
+
+const observer = new IntersectionObserver((entries, observer) => {
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add('show');
+      observer.unobserve(entry.target);
+    }
+  });
+});
+
+window.addEventListener('load', () => {
+  // Enable animations globally
+  document.body.classList.add('js-ready');
+
+  // Observe AFTER animations are enabled
+  document.querySelectorAll('.hidden')
+    .forEach(el => observer.observe(el));
+});
